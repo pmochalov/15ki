99ad1a77d361c2835d76c973b64d0920e3b95bc9
Pavel Mochalov <fik-vam@yandex.ru>
2022-02-21 16:33:54 +0300

    Рабочий вариант готов.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const btnWidth = 80;
 const size = 4;
 let emptyCoords = {}; // координата пустого места
 let numbers = getNumbers();
+let activeTags = [];
 
 
 (function () {
@@ -10,65 +11,113 @@ let numbers = getNumbers();
     tagsField.style.height = `${btnWidth * size}px`;
 
     setEmptyCoords();
-    createGame();    
+    createGame();
+    setActiveTags();
 })();
 
 
 tagsField.addEventListener('click', (event) => {
+
     let el = event.target;
 
-    if(el.classList.contains('tags')) return;
+    if (el.classList.contains('tags') || !el.hasAttribute('isactive')) {
+        return;
+    };
 
-    let coordsOld = el.dataset.coords.split(",");
+    let tagCoords = el.dataset.coords.split(",");
 
     numbers[emptyCoords.stroke][emptyCoords.pos] = +el.innerText;
-    numbers[+coordsOld[1]][+coordsOld[0]] = 0;
+    numbers[+tagCoords[1]][+tagCoords[0]] = 0;
 
     el.style.left = emptyCoords.pos * btnWidth + 'px';
-    el.style.top = emptyCoords.stroke * btnWidth + 'px';    
+    el.style.top = emptyCoords.stroke * btnWidth + 'px';
 
     el.dataset.coords = `${emptyCoords.pos},${emptyCoords.stroke}`;
 
-    emptyCoords.pos = +coordsOld[0];
-    emptyCoords.stroke = +coordsOld[1];
+    emptyCoords.pos = +tagCoords[0];
+    emptyCoords.stroke = +tagCoords[1];
+
+    setActiveTags();
+});
 
 
-    // console.log(numbers);
-})
+function setActiveTags() {
+    let coords = []; // 
+
+    if (emptyCoords.pos - 1 !== -1) {
+        coords.push([emptyCoords.pos - 1, emptyCoords.stroke])
+    }
+
+    if (emptyCoords.pos + 1 !== size) {
+        coords.push([emptyCoords.pos + 1, emptyCoords.stroke])
+    }
+
+    if (emptyCoords.stroke - 1 !== -1) {
+        coords.push([emptyCoords.pos, emptyCoords.stroke - 1])
+    }
+
+    if (emptyCoords.stroke + 1 !== size) {
+        coords.push([emptyCoords.pos, emptyCoords.stroke + 1])
+    }
+
+    for (let tag of tagsField.children) {
+        let tagCoords = tag.dataset.coords.split(","); // [2, 3]
+        let isActive = false;
+        for (let i = 0; i < coords.length; i++) {
+            if (coords[i].join("") == tagCoords.join("")) {
+                isActive = true;
+            }
+        }
+
+        if (isActive) {
+            tag.classList.add('tags__btn_active');
+            tag.setAttribute('isactive', "");
+        } else {
+            tag.classList.remove('tags__btn_active');
+            tag.removeAttribute('isactive');
+        }
+
+    }
+
+}
 
 
 function setEmptyCoords() {
-    for(let i = 0; i < numbers.length; i++) {
+
+    for (let i = 0; i < numbers.length; i++) {
         let pos = numbers[i].indexOf(0);
-        if( pos !== -1) {
+        if (numbers[i].indexOf(0) !== -1) {
             emptyCoords = {
                 pos: pos,
                 stroke: i
             }
         }
     }
+
 }
 
 
 function createGame() {
-    
-    for(let i = 0; i < numbers.length; i++) {        
+
+    for (let i = 0; i < numbers.length; i++) {
         createStroke(numbers[i], i)
     }
+
 }
 
 
 function createStroke(numbers, stroke) {
-    
-    for(let pos = 0; pos < numbers.length; pos++) {
 
-        if(numbers[pos] == 0) {
+    for (let pos = 0; pos < numbers.length; pos++) {
+
+        if (numbers[pos] == 0) {
             continue;
         }
 
         let tag = createTag(numbers[pos], pos, stroke);
         tagsField.append(tag);
     }
+
 }
 
 
@@ -83,22 +132,18 @@ function createTag(number, pos, stroke) {
     btn.style.top = stroke * btnWidth + 'px';
     btn.dataset.coords = `${pos},${stroke}`;
 
-    // if(emptyCoords.pos == pos) {
-    //     btn.classList.add('tags__btn_active')
-    // }
-
     return btn;
 }
 
 
 function getNumbers() {
-    
+
     let numbers = [];
 
-    while(numbers.length < size ** 2) {    
-        let rand = getRandomInt(0, (size**2) - 1);
-    
-        if(!numbers.includes(rand)) {
+    while (numbers.length < size ** 2) {
+        let rand = getRandomInt(0, (size ** 2) - 1);
+
+        if (!numbers.includes(rand)) {
             numbers.push(rand);
         } else {
             continue;
@@ -107,15 +152,14 @@ function getNumbers() {
 
     let randNumbers = [];
 
-    for(let i = 0; i < size; i++) {
-        randNumbers.push(numbers.slice(size*i, size*(i+1)))
+    for (let i = 0; i < size; i++) {
+        randNumbers.push(numbers.slice(size * i, size * (i + 1)))
     }
 
     return randNumbers;
 }
 
 
-// Случайное число
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -124,7 +168,5 @@ function getRandomInt(min, max) {
 }
 
 
-
-// console.log(createNumbers())
 
 // http://127.0.0.1:5500/index.html
